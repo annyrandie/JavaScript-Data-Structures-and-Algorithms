2818ec0133fbeeb262299d9610d46705e0f453dd
Hanna Pereverzieva <41108545+annyrandie@users.noreply.github.com>
2019-11-22 10:57:52 +0200

    Create isPrime.js

---
+++ isPrime.js
@@ -0,0 +1,14 @@
+//check if number isPrime
+
+function isPrime(n) {
+  if (n <= 1) {
+    return false;
+  }
+
+  for (let i = 2; i < n; i++) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
